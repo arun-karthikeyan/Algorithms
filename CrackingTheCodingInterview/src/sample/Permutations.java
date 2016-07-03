7b63caa033859ebc446145030c1601e03b50a3d7
Arun Karthikeyan <akarthi2@asu.edu>
2016-07-03 12:27:12 -0700

    permutations code update to include repetitions

--- CrackingTheCodingInterview/src/sample/Permutations.java
+++ CrackingTheCodingInterview/src/sample/Permutations.java
@@ -5,24 +5,53 @@ import java.io.InputStreamReader;
 
 //	print all permutations of the given string - recursive
 public class Permutations {
-	
-	static void permute(String prefix, String remaining){
+	/**
+	 * Returns all permutations of a given string without repetition
+	 * @param prefix - already permuted prefix
+	 * @param remaining - remaining characters to be permuted
+	 */
+	static void permuteWoRep(String prefix, String remaining){
 		if(remaining.length()==0){
 			System.out.println(prefix);
+			return;
 		}
 		
 		for(int i=0, j=remaining.length(); i<j; ++i){
-			permute(prefix+remaining.charAt(i), remaining.substring(0,i)+remaining.substring(i+1));
+			permuteWoRep(prefix+remaining.charAt(i), remaining.substring(0,i)+remaining.substring(i+1));
+		}
+	}
+	/**
+	 * Returns all permutations of a given string with repetitions
+	 * @param prefix - already permuted prefix
+	 * @param remaining - remaining characters to be permuted
+	 */
+	static void permuteWRep(String prefix, String remaining){
+		if(prefix.length()==remaining.length()){
+			System.out.println(prefix);
+			return;
+		}
+		
+		for(int i=0, j=remaining.length(); i<j; ++i){
+			permuteWRep(prefix+remaining.charAt(i), remaining);
 		}
 	}
 	
 	public static void main(String[] args) throws Exception {
 		String str;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter 1 to permute with repetitions");
+		System.out.println("Enter 2 to permute without repetitions");
+		int choice = Integer.parseInt(br.readLine());
 		
 		System.out.println("Enter the string to permute");
 		str = br.readLine();
-		permute("",str);
+		
+		if(choice==1){
+			permuteWRep("", str);
+		}else{			
+			permuteWoRep("",str);
+		}
+		
 		
 	}
 }
