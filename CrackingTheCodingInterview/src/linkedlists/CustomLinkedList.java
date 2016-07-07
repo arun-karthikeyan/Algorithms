a49990fb0925b0cc6b3481d1e9936dc1cc88eaeb
Arun Karthikeyan <akarthi2@asu.edu>
2016-07-06 18:50:31 -0700

    more linkedlist problems

--- CrackingTheCodingInterview/src/linkedlists/CustomLinkedList.java
+++ CrackingTheCodingInterview/src/linkedlists/CustomLinkedList.java
@@ -1,6 +1,7 @@
 package linkedlists;
 
 import java.util.HashSet;
+import java.util.Stack;
 
 public class CustomLinkedList<T extends Number & Comparable<T>> {
 
@@ -10,6 +11,17 @@ public class CustomLinkedList<T extends Number & Comparable<T>> {
 	class Node<E extends Number & Comparable<E>>{
 		Node<E> next;
 		E data;
+		public Node(){
+			this.data = null;
+			this.next = null;
+		}
+		public Node(E data){
+			this.data = data;
+			this.next = null;
+		}
+//		public String toString(){
+//			return "data: "+this.data+"; next: [ "+this.next+" ]";
+//		}
 	}
 	
 	public CustomLinkedList(){
@@ -23,6 +35,21 @@ public class CustomLinkedList<T extends Number & Comparable<T>> {
 		newNode.next = null;
 		this.Head = newNode;
 		this.size = 1;
+	}
+	
+	public void append(CustomLinkedList<T>.Node<T> node){
+		if(this.isEmpty()){
+			this.Head = node;
+			
+		}
+		else{
+		Node<T> tempHead = this.getHead();
+		while(tempHead.next!=null){
+			tempHead = tempHead.next;
+		}
+		tempHead.next = node;
+		}
+		this.size++;
 	}
 	
 	public void append(T data){
@@ -83,21 +110,21 @@ public class CustomLinkedList<T extends Number & Comparable<T>> {
 		return size==0;
 	}
 	
-	public String toString(){
-		StringBuilder sb = new StringBuilder();
-		sb.append("[");
-		if(size!=0){
-			Node<T> tempHead = this.Head;
-			sb.append(tempHead.data);
-			tempHead = tempHead.next;
-			while(tempHead!=null){
-				sb.append(", ").append(tempHead.data);
-				tempHead = tempHead.next;
-			}
-		}
-		sb.append("]");
-		return sb.toString();
-	}
+//	public String toString(){
+//		StringBuilder sb = new StringBuilder();
+//		sb.append("[");
+//		if(size!=0){
+//			Node<T> tempHead = this.Head;
+//			sb.append(tempHead.data);
+//			tempHead = tempHead.next;
+//			while(tempHead!=null){
+//				sb.append(", ").append(tempHead.data);
+//				tempHead = tempHead.next;
+//			}
+//		}
+//		sb.append("]");
+//		return sb.toString();
+//	}
 	public Node<T> getHead(){
 		return this.Head;
 	}
@@ -304,10 +331,11 @@ public class CustomLinkedList<T extends Number & Comparable<T>> {
 		result[nodeIdx] = ((Node<T>)result[nodeIdx]).next;
 		return result;
 	}
+	
 	//linked list palindrome check - two pass
 	public boolean isPalindromeTwoPass(){
 		int size = 0; //assuming size is not given
-		CustomLinkedList ll = new CustomLinkedList();
+		CustomLinkedList<T> ll = new CustomLinkedList<T>();
 		Node<T> tempHead = this.getHead();
 		while(tempHead!=null){
 			ll.appendToHead(tempHead.data);
@@ -326,13 +354,217 @@ public class CustomLinkedList<T extends Number & Comparable<T>> {
 		}
 		return true;
 	}
-	public static void main(String[] args) throws Exception{
+	
+	//getRecursive middle
+	public boolean isPalindromeAlternate(){
+		return (Boolean)this.getRecursiveNextToMiddle(this.getHead(), 0)[2];
+	}
+	private Object[] getRecursiveNextToMiddle(Node<T> tempHead,int length){
+		if(tempHead==null){
+			return new Object[]{new Integer(length),null, true};
+		}
+		Object[] returns = getRecursiveNextToMiddle(tempHead.next, length+1);
+		if((Integer)returns[0]==1){
+			//reached middle
+			returns[1] = tempHead;
+		}
+		if((Integer)returns[0]==0){
+			//reached middle
+			returns[1] = tempHead.next;
+		}
+		if(returns[1]!=null){
+			returns[2] = (Boolean)returns[2] & tempHead.data.compareTo(((Node<T>)returns[1]).data)==0;
+			returns[1] = ((Node<T>)returns[1]).next;
+		}
+		returns[0] = (Integer)returns[0]-2;
+		return returns;
+	}
+	
+	//O(N^2) - sucks
+	public Node<T> getIntersectingNodeNaive(CustomLinkedList<T> ll){
+		if(this.size==ll.size){
+			return null;
+		}
+		
+		Node<T> longerHead = this.size>ll.size?this.getHead():ll.getHead();
+		Node<T> shorterHead = this.size<ll.size?this.getHead():ll.getHead();
+		
+		while(longerHead!=null){
+			Node<T> tempHead = shorterHead;
+			while(tempHead!=null){
+				if(longerHead==tempHead){
+					return tempHead;
+				}
+				tempHead = tempHead.next;
+			}
+			longerHead = longerHead.next;
+		}
+		
+		return null;
+	}
+	
+	//O(N) - pretty decent - assuming length not given
+	public Node<T> getIntersectingNode(CustomLinkedList<T> ll){
+		Object[] prop1 = this.getTailAndSize(this.getHead());
+		Object[] prop2 = ll.getTailAndSize(ll.getHead());
+		
+		//if tails don't match, return null
+		if(prop1[0]!=prop2[0]){
+			return null;
+		}
+		
+		Node<T> longerHead = (Integer)prop1[1]>(Integer)prop2[1]?this.getHead():ll.getHead();
+		Node<T> shorterHead = (Integer)prop1[1]<(Integer)prop2[1]?this.getHead():ll.getHead();
+		
+		int chipOff = Math.abs((Integer)prop1[1]-(Integer)prop2[1]);
+		
+		while(chipOff-->0){
+			longerHead = longerHead.next;
+		}
+		
+		while(shorterHead!=longerHead){
+			longerHead = longerHead.next;
+			shorterHead = shorterHead.next;
+		}
+		
+		return shorterHead;
+	}
+	private Object[] getTailAndSize(Node<T> head1){
+		int size = 1;
+		Node<T> tail = head1;
+		while(tail.next!=null){
+			tail = tail.next;
+			size++;
+		}
+		return new Object[]{tail, size};
+	}
+	
+	public void getIntersectingNodeDemo(){
+		CustomLinkedList<Integer> ll1 = new CustomLinkedList<Integer>();
+		ll1.append(3);
+		ll1.append(1);
+		ll1.append(5);
+		ll1.append(9);
+		
+		CustomLinkedList<Integer> inter = new CustomLinkedList<Integer>();
+		inter.append(7);
+		inter.append(2);
+		inter.append(1);
+		
+		CustomLinkedList<Integer> ll2 = new CustomLinkedList<Integer>();
+		ll2.append(4);
+		ll2.append(6);
+		
+		ll1.appendAll(inter);
+		ll2.appendAll(inter);
+		
+		CustomLinkedList<Integer>.Node<Integer> interNode = ll1.getIntersectingNode(ll2);
+		System.out.println(interNode);
+	}
+	
+	//naive approach - uses more space
+	public Node<T> getLoopStartInCircularLinkedListNaive(){
+		if(this.getHead()==null){
+			return null;
+		}
+		Stack<Node<T>> dfs = new Stack<Node<T>>();
+		dfs.push(this.getHead());
+		HashSet<Node<T>> visitedSet = new HashSet<Node<T>>();
+		while(!dfs.isEmpty()){
+			Node<T> currentNode = dfs.pop();
+			if(!visitedSet.add(currentNode)){
+				//already visited
+				return currentNode;
+			}
+			if(currentNode.next!=null)
+			{
+				dfs.push(currentNode.next);
+			}
+			
+		}
+		return null;
+	}
+	//make sure to comment out the toString methods from CustomLinkedList<T> and Node<T> to avoid stackoverflow
+	public void getLoopDemo(){
 		CustomLinkedList<Integer> ll = new CustomLinkedList<Integer>();
-		ll.append(1);
-		ll.append(2);
-		ll.append(3);
-		ll.append(2);
-		ll.append(1);
-		System.out.println(ll.isPalindromeTwoPass());
+		
+		CustomLinkedList<Integer>.Node<Integer> one = ll.new Node<Integer>(1);
+		CustomLinkedList<Integer>.Node<Integer> two = ll.new Node<Integer>(2);
+		CustomLinkedList<Integer>.Node<Integer> three = ll.new Node<Integer>(3);
+		CustomLinkedList<Integer>.Node<Integer> four = ll.new Node<Integer>(4);
+		CustomLinkedList<Integer>.Node<Integer> five = ll.new Node<Integer>(5);
+		CustomLinkedList<Integer>.Node<Integer> six = ll.new Node<Integer>(6);
+		
+		ll.append(one);
+		ll.append(two);
+		ll.append(three);
+		ll.append(four);
+		ll.append(five);
+		ll.append(six);
+		ll.append(three);
+		
+//		System.out.println(ll.getLoopStartInCircularLinkedListSmart().data);
+//		System.out.println(ll.containsLoop());
+		System.out.println(ll.getLoopSizeInCircularLinkedListSmart());
+		
+	}
+	
+	public boolean containsLoop(){
+		if(this.isEmpty() || this.size<2){
+			return false;
+		}
+		Node<T> slowRunner = this.getHead();
+		Node<T> fastRunner = this.getHead();
+		while(slowRunner!=null && fastRunner!=null && fastRunner.next!=null){
+			slowRunner = slowRunner.next;
+			fastRunner = fastRunner.next.next;
+			if(slowRunner == fastRunner){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	//according to cracking the coding interview approach
+	public Node<T> getLoopStartInCircularLinkedListSmart(){
+		Node<T> slowRunner = this.getHead();
+		Node<T> fastRunner = this.getHead();
+		
+		while(slowRunner!=null && fastRunner!=null && fastRunner.next!=null){
+			slowRunner=slowRunner.next;
+			fastRunner = fastRunner.next.next;
+			if(slowRunner==fastRunner){
+				break;
+			}
+		}
+		
+		fastRunner = this.getHead();
+		while(slowRunner!=null && fastRunner!=null){
+			slowRunner = slowRunner.next;
+			fastRunner = fastRunner.next;
+			if(slowRunner==fastRunner){
+				break;
+			}
+		}
+		return slowRunner;
+	}
+	
+	//getting the loopsize - a variation
+	public int getLoopSizeInCircularLinkedListSmart(){
+		Node<T> slowRunner = this.getHead();
+		Node<T> fastRunner = this.getHead();
+		int loopSize=0;
+		while(slowRunner!=null && fastRunner!=null && fastRunner.next!=null){
+			loopSize++;
+			slowRunner = slowRunner.next;
+			fastRunner = fastRunner.next.next;
+			if(slowRunner==fastRunner){
+				break;
+			}
+		}
+		return loopSize;
+	}
+	public static void main(String[] args) throws Exception{
+		new CustomLinkedList<Integer>().getLoopDemo();
 	}
 }
