96f51283d53b44a226be4a3c4212835af812a6d2
Arun Karthikeyan <akarthi2@asu.edu>
2016-12-12 18:43:24 -0700

    CD

---
+++ CompetitiveProgramming/UVA11849_2.java
@@ -0,0 +1,115 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class UVA11849_2 {
+private static int totalchars = 0, offset = 0;
+private static InputStream stream;
+private static byte[] buffer = new byte[1024];
+
+private static int readByte() {
+	if (totalchars < 0)
+		return 0;
+	if (offset >= totalchars) {
+		offset = 0;
+		try {
+			totalchars = stream.read(buffer);
+		} catch (IOException e) {
+			return 0;
+		}
+		if (totalchars <= 0)
+			return -1;
+	}
+	return buffer[offset++];
+}
+
+private static int readInt() {
+	int number = readByte();
+
+	while (eolchar(number))
+		number = readByte();
+
+	int sign = 1;
+	int val = 0;
+
+	if (number == '-') {
+		sign = -1;
+		number = readByte();
+	}
+
+	do {
+		if ((number < '0') || (number > '9'))
+			return 0;
+		val *= 10;
+		val += (number - '0');
+		number = readByte();
+	} while (!eolchar(number));
+
+	return sign * val;
+}
+
+private static long readLong() {
+	int number = readByte();
+
+	while (eolchar(number))
+		number = readByte();
+
+	int sign = 1;
+	long val = 0;
+
+	if (number == '-') {
+		sign = -1;
+		number = readByte();
+	}
+
+	do {
+		if ((number < '0') || (number > '9')) {
+			//return sign*val;
+			return 0;
+		}
+		val *= 10;
+		val += (number - '0');
+		number = readByte();
+	} while (!eolchar(number));
+
+	return sign * val;
+}
+
+private static boolean eolchar(int c) {
+	return c == ' ' || c == '\n' || c == -1 || c == '\r' || c == '\t';
+}
+
+public static void main(String[] args) throws Exception {
+	if (args.length > 0 && "fileip".equals(args[0])) {
+		stream = new FileInputStream(new File("testip.txt"));
+	} else {
+		stream = System.in;
+	}
+
+	PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+
+	int n,m;
+	while((n=readInt())!=0 && (m=readInt())!=0){
+		int count = 0;
+		int[] jack = new int[n];
+		for(int i=0; i<n; ++i){
+			jack[i] = readInt();
+		}
+		Arrays.sort(jack);
+		for(int i=0; i<m; ++i){
+			if(Arrays.binarySearch(jack, readInt())>=0){
+				count++;
+			}
+		}
+		pw.println(count);
+	}
+
+	pw.flush();
+	pw.close();
+}
+}
