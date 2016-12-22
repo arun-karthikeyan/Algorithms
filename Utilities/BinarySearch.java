411bff5acf46582e13bb5f8b9720c1cc7afc3f3e
Arun Karthikeyan <akarthi2@asu.edu>
2016-12-22 14:55:15 -0700

    updated BinarySearch.java

--- Utilities/BinarySearch.java
+++ Utilities/BinarySearch.java
@@ -1,16 +1,15 @@
 public class BinarySearch<E extends Comparable<E>> {
 
 	/**
-	 * Binary Search to find the first occurrence of a value in the given array
+	 * Binary Search to find the first occurrence of the key in the given array
 	 * 
 	 * @param array
 	 *            sorted array on which the search has to be performed
-	 * @param val
+	 * @param key
 	 *            the key which has to be searched for
-	 * @return pos if key exists or -pos if the key doesn't exist but where it
-	 *         would've been inserted in this array
+	 * @return pos if key exists or -1 if key doesn't exist
 	 */
-	public int firstOccurenceBinarySearch(E[] array, E val) {
+	public int firstOccurrenceBinarySearch(E[] array, E key) {
 		int len = array == null ? 0 : array.length;
 		if (len == 0) {
 			return -1;
@@ -18,26 +17,25 @@ public class BinarySearch<E extends Comparable<E>> {
 		int low = 0, high = len - 1;
 		while (low <= high) {
 			int mid = low + ((high - low) >> 1);
-			if (array[mid].compareTo(val) <= 0) {
+			if (key.compareTo(array[mid]) <= 0) {
 				high = mid - 1;
 			} else {
 				low = mid + 1;
 			}
 		}
-		return array[low].compareTo(val) == 0 ? low : -low;
+		return low < len && array[low].compareTo(key) == 0 ? low : -1;
 	}
 
 	/**
-	 * Binary Search to find the last occurrence of a value in the given array
+	 * Binary Search to find the last occurrence of they key in the given array
 	 * 
 	 * @param array
 	 *            sorted array on which the search has to be performed
-	 * @param val
+	 * @param key
 	 *            the key which has to be searched for
-	 * @return pos if key exists or -pos if the key doesn't exist but where it
-	 *         would've been inserted in this array
+	 * @return pos if key exists or -1 if key doesn't exist
 	 */
-	public int lastOccurrenceBinarySearch(E[] array, E val) {
+	public int lastOccurrenceBinarySearch(E[] array, E key) {
 		int len = array == null ? 0 : array.length;
 		if (len == 0) {
 			return -1;
@@ -45,25 +43,25 @@ public class BinarySearch<E extends Comparable<E>> {
 		int low = 0, high = len - 1;
 		while (low <= high) {
 			int mid = low + ((high - low) >> 1);
-			if (array[mid].compareTo(val) < 0) {
+			if (key.compareTo(array[mid]) < 0) {
 				high = mid - 1;
 			} else {
 				low = mid + 1;
 			}
 		}
-		return array[low].compareTo(val) == 0 ? low : -low;
+		return high > -1 && array[high].compareTo(key) == 0 ? high : -1;
 	}
 
 	/**
-	 * Binary Search to find if a value is present in the given sorted array
+	 * Binary Search to find if the key is present in the given sorted array
 	 * 
 	 * @param array
 	 *            sorted array on which the search has to be performed
-	 * @param val
+	 * @param key
 	 *            the key which has to be searched for
 	 * @return pos if key exists or -1 if the key doesn't exist in this array
 	 */
-	public int binarySearch(E[] array, E val) {
+	public int binarySearch(E[] array, E key) {
 		int len = array == null ? 0 : array.length;
 		if (len == 0) {
 			return -1;
@@ -71,7 +69,7 @@ public class BinarySearch<E extends Comparable<E>> {
 		int low = 0, high = len - 1;
 		while (low <= high) {
 			int mid = low + ((high - low) >> 1);
-			int compare = array[mid].compareTo(val);
+			int compare = key.compareTo(array[mid]);
 			if (compare < 0) {
 				high = mid - 1;
 			} else if (compare > 0) {
@@ -81,6 +79,73 @@ public class BinarySearch<E extends Comparable<E>> {
 			}
 		}
 		return -1;
+	}
+
+	/**
+	 * Binary search to find the first position where the given key can be
+	 * inserted in the sorted array
+	 * 
+	 * @param array
+	 *            sorted array on which the search has to be performed
+	 * @param key
+	 *            the key which has to be inserted
+	 * @return first possible insert position of key in the given array
+	 */
+	public int firstInsertPosition(E[] array, E key) {
+		int len = array == null ? 0 : array.length;
+		if (len == 0) {
+			return -1;
+		}
+		int low = 0, high = len - 1;
+		while (low <= high) {
+			int mid = low + ((high - low) >> 1);
+			;
+			if (key.compareTo(array[mid]) <= 0) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return low;
+	}
+
+	/**
+	 * Binary search to find the last position where the given key can be
+	 * inserted in the sorted array
+	 * 
+	 * @param array
+	 *            sorted array on which the search has to be performed
+	 * @param key
+	 *            the key which has to be inserted
+	 * @return last possible insert position of key in the given array
+	 */
+	public int lastInsertPosition(E[] array, E key) {
+		int len = array == null ? 0 : array.length;
+		if (len == 0) {
+			return -1;
+		}
+		int low = 0, high = len - 1;
+		while (low <= high) {
+			int mid = low + ((high - low) >> 1);
+			if (key.compareTo(array[mid]) < 0) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return low;
+	}
+
+	public static void main(String[] args) {
+		BinarySearch<Integer> bs = new BinarySearch<Integer>();
+		Integer[] array = new Integer[] { 1, 2, 3, 4, 4, 4, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 10, 11, 12, 13, 13, 13,
+				1000 };
+		Integer val = 13;
+		System.out.println("First occurrence of " + val + " is " + bs.firstOccurrenceBinarySearch(array, val));
+		System.out.println("Last occurrence of " + val + " is " + bs.lastOccurrenceBinarySearch(array, val));
+		System.out.println("First insert position of " + val + " is " + bs.firstInsertPosition(array, val));
+		System.out.println("Last insert position of " + val + " is " + bs.lastInsertPosition(array, val));
+		System.out.println("Is " + val + " present in the array ? " + (bs.binarySearch(array, val) > 0));
 	}
 
 }
