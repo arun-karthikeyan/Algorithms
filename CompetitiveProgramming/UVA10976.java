3ebd5f8d38cba075cc4a7039b8b17c4b43364fba
Arun Karthikeyan <akarthi2@asu.edu>
2016-12-28 14:17:01 -0700

    One Loop problems

---
+++ CompetitiveProgramming/UVA10976.java
@@ -0,0 +1,128 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+/**
+ * Fractions again
+ * @author arun
+ *
+ */
+public class UVA10976 {
+	private static int totalchars = 0, offset = 0;
+	private static InputStream stream;
+	private static byte[] buffer = new byte[1024];
+
+	private static int readByte() {
+		if (totalchars < 0)
+			return 0;
+		if (offset >= totalchars) {
+			offset = 0;
+			try {
+				totalchars = stream.read(buffer);
+			} catch (IOException e) {
+				return 0;
+			}
+			if (totalchars <= 0)
+				return -1;
+		}
+		return buffer[offset++];
+	}
+
+	private static int readInt() {
+		int number = readByte();
+
+		while (eolchar(number))
+			number = readByte();
+
+		int sign = 1;
+		int val = 0;
+
+		if (number == '-') {
+			sign = -1;
+			number = readByte();
+		}
+
+		do {
+			if ((number < '0') || (number > '9'))
+				return 0;
+			val *= 10;
+			val += (number - '0');
+			number = readByte();
+		} while (!eolchar(number));
+
+		return sign * val;
+	}
+
+	private static long readLong() {
+		int number = readByte();
+
+		while (eolchar(number))
+			number = readByte();
+
+		int sign = 1;
+		long val = 0;
+
+		if (number == '-') {
+			sign = -1;
+			number = readByte();
+		}
+
+		do {
+			if ((number < '0') || (number > '9')) {
+				// return sign*val;
+				return 0;
+			}
+			val *= 10;
+			val += (number - '0');
+			number = readByte();
+		} while (!eolchar(number));
+
+		return sign * val;
+	}
+
+	private static boolean eolchar(int c) {
+		return c == ' ' || c == '\n' || c == -1 || c == '\r' || c == '\t';
+	}
+
+	static class Pair {
+		int x, y;
+
+		public Pair(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		if (args.length > 0 && "fileip".equals(args[0])) {
+			stream = new FileInputStream(new File("testip.txt"));
+		} else {
+			stream = System.in;
+		}
+
+		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+
+		int k;
+
+		while ((k = readInt()) != 0) {
+			ArrayList<Pair> sol = new ArrayList<Pair>();
+			for (int i = k + 1, iLen = 2 * k; i <= iLen; ++i) {
+				int num = k * i, den = i - k;
+				if (num % den == 0) {
+					sol.add(new Pair(num / den, i));
+				}
+			}
+			pw.println(sol.size());
+			for (int i = 0, iLen = sol.size(); i < iLen; ++i) {
+				Pair currentPair = sol.get(i);
+				pw.println("1/" + k + " = 1/" + currentPair.x + " + " + "1/" + currentPair.y);
+			}
+		}
+
+		pw.flush();
+		pw.close();
+	}
+}
