2d50b875eead26423fa6e8215a99210ec4b42f16
Arun Karthikeyan <akarthi2@asu.edu>
2017-01-13 21:13:38 -0700

    updated bitmasks

--- CompetitiveProgramming/UVA10718.java
+++ CompetitiveProgramming/UVA10718.java
@@ -1,9 +1,62 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 public class UVA10718 {
+	private static int totalchars = 0, offset = 0;
+	private static InputStream stream;
+	private static byte[] buffer = new byte[1024];
+
+	private static int readByte() {
+		if (totalchars < 0)
+			return 0;
+		if (offset >= totalchars) {
+			offset = 0;
+			try {
+				totalchars = stream.read(buffer);
+			} catch (IOException e) {
+				return 0;
+			}
+			if (totalchars <= 0)
+				return -1;
+		}
+		return buffer[offset++];
+	}
+
+	private static long readLong() {
+		int number = readByte();
+
+		while (eolchar(number))
+			number = readByte();
+
+		int sign = 1;
+		long val = 0;
+
+		if (number == '-') {
+			sign = -1;
+			number = readByte();
+		}
+
+		do {
+			if ((number < '0') || (number > '9')) {
+				// return sign*val;
+				return -1;
+			}
+			val *= 10;
+			val += (number - '0');
+			number = readByte();
+		} while (!eolchar(number));
+
+		return sign * val;
+	}
+
+	private static boolean eolchar(int c) {
+		return c == ' ' || c == '\n' || c == -1 || c == '\r' || c == '\t';
+	}
+
 	private static long getOKBits(long n) {
 		for (long val = 1; val < n; val *= 2) {
 			n |= val;
@@ -12,13 +65,22 @@ public class UVA10718 {
 	}
 
 	public static void main(String[] args) throws Exception {
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		BufferedReader br = new BufferedReader(new FileReader("testip.txt"));
+		if (args.length > 0 && "fileip".equals(args[0])) {
+			stream = new FileInputStream(new File("testip.txt"));
+		} else {
+			stream = System.in;
+		}
+
 		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
-		String line;
-		while ((line = br.readLine()) != null) {
-			String[] info = line.split(" ");
-			long n = Long.parseLong(info[0]), l = Long.parseLong(info[1]), u = Long.parseLong(info[2]);
+
+		long n, l, u;
+		while (true) {
+			n = readLong();
+			l = readLong();
+			u = readLong();
+			if (n == -1 && u == -1 && l == -1) {
+				break;
+			}
 			long ok2 = getOKBits(u);
 			long bitsToChange = (ok2 & (((1l << 32) - 1) & ~n));
 			String bitsToChangeStr = Long.toBinaryString(bitsToChange);
@@ -47,9 +109,7 @@ public class UVA10718 {
 
 		}
 
-		br.close();
 		pw.flush();
 		pw.close();
 	}
-
 }
