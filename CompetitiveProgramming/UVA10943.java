00a1b04d13f31817ae13529df7cf63b63140ab2f
Arun Karthikeyan <akarthi2@asu.edu>
2017-01-18 04:14:17 -0700

    adding up numbers

---
+++ CompetitiveProgramming/UVA10943.java
@@ -0,0 +1,116 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+public class UVA10943 {
+	private static int totalchars = 0, offset = 0;
+	private static InputStream stream;
+	private static byte[] buffer = new byte[1024];
+
+	private static int readByte() {
+		if (totalchars < 0)
+			return 0;
+		if (offset >= totalchars) {
+			offset = 0;
+			try {
+				totalchars = stream.read(buffer);
+			} catch (IOException e) {
+				return 0;
+			}
+			if (totalchars <= 0)
+				return -1;
+		}
+		return buffer[offset++];
+	}
+
+	private static int readInt() {
+		int number = readByte();
+
+		while (eolchar(number))
+			number = readByte();
+
+		int sign = 1;
+		int val = 0;
+
+		if (number == '-') {
+			sign = -1;
+			number = readByte();
+		}
+
+		do {
+			if ((number < '0') || (number > '9'))
+				return 0;
+			val *= 10;
+			val += (number - '0');
+			number = readByte();
+		} while (!eolchar(number));
+
+		return sign * val;
+	}
+
+	private static long readLong() {
+		int number = readByte();
+
+		while (eolchar(number))
+			number = readByte();
+
+		int sign = 1;
+		long val = 0;
+
+		if (number == '-') {
+			sign = -1;
+			number = readByte();
+		}
+
+		do {
+			if ((number < '0') || (number > '9')) {
+				// return sign*val;
+				return 0;
+			}
+			val *= 10;
+			val += (number - '0');
+			number = readByte();
+		} while (!eolchar(number));
+
+		return sign * val;
+	}
+
+	private static boolean eolchar(int c) {
+		return c == ' ' || c == '\n' || c == -1 || c == '\r' || c == '\t';
+	}
+
+	public static void main(String[] args) throws Exception {
+		if (args.length > 0 && "fileip".equals(args[0])) {
+			stream = new FileInputStream(new File("testip.txt"));
+		} else {
+			stream = System.in;
+		}
+
+		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+		int nMax = 201;
+		int[][] ncr = new int[nMax + 1][nMax + 1];
+		ncr[0][0] = 1;
+		int mod = (int) 1e6;
+		for (int i = 1; i <= nMax; ++i) {
+			ncr[i][0] = 1;
+			for (int j = 1; j <= i; ++j) {
+				ncr[i][j] = (ncr[i - 1][j] + ncr[i - 1][j - 1])%mod;
+			}
+		}
+		int n, k;
+		while (true) {
+			n = readInt();
+			k = readInt();
+			if (n == 0 && k == 0) {
+				break;
+			}
+			pw.println(ncr[n + k - 1][k - 1]);
+		}
+
+		pw.flush();
+		pw.close();
+	}
+}
